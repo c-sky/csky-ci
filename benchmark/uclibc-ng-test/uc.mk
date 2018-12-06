22efcd9ae5ccbbf11be76260df4ffa7ba31a886c
Mao Han <han_mao@c-sky.com>
2018-12-06 14:21:29 +0800

    Update uc.mk

--- benchmark/uclibc-ng-test/uc.mk
+++ benchmark/uclibc-ng-test/uc.mk
@@ -1,7 +1,7 @@
 UCLIBC-NG-TEST_RUN_TEST=uc_run_test
 UCLIBC-NG-TEST_PARSE_RESULT=uc_parse_result
 
-define LTP_CP_CONFIGS
+define UCLIBC-NG-TEST_CP_CONFIGS
     cp benchmark/uclibc-ng-test/uclibc-ng-test.skiplist \
        $(ROOTDIR)/out/configs/
 endef
