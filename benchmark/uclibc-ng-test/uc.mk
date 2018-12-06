62b1aeb7d3109f310408a1c639993cb79ef64d96
Mao Han <han_mao@c-sky.com>
2018-12-06 14:19:37 +0800

    add uclibc skip list

--- benchmark/uclibc-ng-test/uc.mk
+++ benchmark/uclibc-ng-test/uc.mk
@@ -1,2 +1,7 @@
 UCLIBC-NG-TEST_RUN_TEST=uc_run_test
 UCLIBC-NG-TEST_PARSE_RESULT=uc_parse_result
+
+define LTP_CP_CONFIGS
+    cp benchmark/uclibc-ng-test/uclibc-ng-test.skiplist \
+       $(ROOTDIR)/out/configs/
+endef
