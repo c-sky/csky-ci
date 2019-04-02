371170091c75cf94e34901826f7e9c5bb00bd990
Liu Yibin <yibin_liu@c-sky.com>
2019-04-02 20:44:12 +0800

    Show no testcases when no testcases
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- generic/test.sh
+++ generic/test.sh
@@ -5,14 +5,20 @@ sleep 5
 date -s 09:00:00
 sleep 10 # Sleep 10 more secs to login
 
-cd /etc/init.ci/
-for i in *; do
-	j=${i%_*}
-	echo ================== $j test start ==================
-	./$i
-	echo ================== $j test end ==================
-done
-cd - #This will echo a "/", so, don't bother
+num=`ls -A /etc/init.ci/ | wc -w`
+
+if [ $num == 0 ]; then
+	echo "No test cases"
+else
+	cd /etc/init.ci/
+	for i in *; do
+		j=${i%_*}
+		echo ================== $j test start ==================
+		./$i
+		echo ================== $j test end ==================
+	done
+	cd - #This will echo a "/", so, don't bother
+fi
 
 if [ -f /usr/lib/csky-ci/total_result ]; then
 	echo "csky-ci tests failed"
