624cef8b72717051765700c5c11602717e8da1ba
Mao Han <han_mao@c-sky.com>
2019-09-26 09:14:41 +0800

    add more delay for target ssh startup and fix log comment

--- generic/check_ssh_bg.sh
+++ generic/check_ssh_bg.sh
@@ -1,6 +1,6 @@
 rm ~/.ssh/known_hosts -f
-sleep 60
-echo ================== perf test start ================== > ssh.log
+sleep 120
+echo ================== ssh test start ================== > ssh.log
 ssh -o StrictHostKeyChecking=no $1 -p 5022 ls / >> ssh.log
 ssh -o StrictHostKeyChecking=no $1 -p 5022 echo "ssh check pass!" >> ssh.log
-echo ================== perf test end ================== >> ssh.log
+echo ================== ssh test end ================== >> ssh.log
