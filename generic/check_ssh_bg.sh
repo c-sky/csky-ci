25676795658141a39e3e69c8d9c97a02766ad9d5
Mao Han <han_mao@c-sky.com>
2019-09-27 14:29:16 +0800

    use virtio-rng to speed up boot up

--- generic/check_ssh_bg.sh
+++ generic/check_ssh_bg.sh
@@ -1,5 +1,5 @@
 rm ~/.ssh/known_hosts -f
-sleep 120
+sleep 30
 echo ================== ssh test start ================== > ssh.log
 ssh -o StrictHostKeyChecking=no $1 -p 5022 ls / >> ssh.log
 ssh -o StrictHostKeyChecking=no $1 -p 5022 echo "ssh check pass!" >> ssh.log
